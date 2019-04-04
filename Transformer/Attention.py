2f2c683e91b2531b3d20ff2c98e557036f4e571d
shashwattrivedi <shashwattrivedi6.5.97@gmail.com>
2019-04-04 14:00:19 +0530

    Added few comments

--- Transformer/Attention.py
+++ Transformer/Attention.py
@@ -6,7 +6,14 @@ import numpy as np
 
 
 class MultiHeadedAttention():
-
+    '''
+    Self-Attention layer of Transformer block described in "Attention is all you need".
+    d_model: the output dimensions for embeddings of each token in the sequence.
+    heads  : number of heads 
+    dim_q  : query vector and key vector dimensions
+    dim_v  : value vector dimensions
+    dropout_rate : dropout rate before fully connected layer
+    '''
     def __init__(self,d_model ,heads,dim_q,dim_v,dropout_rate,name, **kwargs):
         self.heads=heads
         self.dim_q=dim_q
@@ -20,7 +27,7 @@ class MultiHeadedAttention():
 
         self.output_layer  = TimeDistributed(Dense(d_model,use_bias = False),name = name+'_AttentionOutputLayer')
 
-
+# moves up the timestep dimention towards the end and heads towards batch_size dimention
     def shift_timestep_ahead(self,vec,last_dim,time_steps,transpose=False):
       vec     = Reshape([-1,self.heads,last_dim])(vec)
 
@@ -34,6 +41,7 @@ class MultiHeadedAttention():
 
       return vec
 
+# moves down the timestep dimention towards the batch_size dimention
     def shift_timestep_behind(self,vec,last_dim,time_steps):
       vec    = Lambda(lambda x:K.reshape(x, [-1,self.heads, time_steps,  last_dim ]))(vec)
       vec    = Permute([2,1,3])(vec)
@@ -43,10 +51,12 @@ class MultiHeadedAttention():
       return vec
 
     def scaledDotAttention(self):
+        '''
+        Scaled dot attention : computes the attention score by dot product of query vector and 
+        key vector. Sums the value vectors of tokens based on attention weights( after softmax of attention score )
+        '''
       attention_score   = Lambda( lambda x: K.batch_dot(x[0],x[1]) / np.sqrt(self.dim_q))([self.query_vec,self.key_vec])
-      
       attention_weights = Activation('softmax')(attention_score)
-
       attention_vector  = Lambda( lambda x: K.batch_dot(x[0],x[1]))([attention_weights,self.value_vec])
 
       return attention_vector, attention_weights
@@ -54,9 +64,7 @@ class MultiHeadedAttention():
     def __call__(self, x):
 
         a = x
-        # print(x,x.get_shape())
         time_steps = int(a.get_shape()[1])
-        # print("time steps = ",time_steps)
         self.query_vec = self.query_layer(a)
         self.key_vec   = self.key_layer(a)
         self.value_vec = self.value_layer(a)
@@ -76,7 +84,7 @@ class MultiHeadedAttention():
         #Output Fully Connected Dense
         temp_out               = self.shift_timestep_behind(self.attention_vec,self.dim_v,time_steps)
         temp_out               = self.dropout_layer(temp_out)
-        self.out_vec   = self.output_layer(temp_out)
+        self.out_vec           = self.output_layer(temp_out)
 
         return [self.out_vec,self.attention_weights]
 
