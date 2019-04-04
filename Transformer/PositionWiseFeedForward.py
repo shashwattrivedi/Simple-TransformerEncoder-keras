1e2364cff0ad6a702ebce890e52b5ed955aae33f
shashwattrivedi <shashwattrivedi6.5.97@gmail.com>
2019-04-04 14:07:55 +0530

    Added comment

--- Transformer/PositionWiseFeedForward.py
+++ Transformer/PositionWiseFeedForward.py
@@ -1,27 +1,24 @@
-from keras import backend as K
-from keras.engine.topology import Layer
 from keras.layers import TimeDistributed,Dense,Dropout
-import math
 import keras
-class PositionWiseFeedForward():
 
+class PositionWiseFeedForward():
+    '''
+        Applies a position wise feed forward dense layer on each token embedding
+    '''
     def __init__(self,d_model,inner_dim,dropout_rate,name, **kwargs):
-        self.inner_dim = inner_dim
+        
+        self.inner_dim     = inner_dim
 
         self.hidden_layer  = TimeDistributed(Dense(self.inner_dim,activation=keras.activations.relu),name = name+'_PositionWiseFeedForward')
         self.dropout_layer = Dropout(dropout_rate)
-
         self.out_layer     = TimeDistributed(Dense(d_model),name = name+'_outffn')
 
      
     def __call__(self, x):
-        # assert isinstance(x, list)
 
         self.hidden_vec = self.hidden_layer(x)
         self.hidden_vec = self.dropout_layer(self.hidden_vec)
-        
         self.out_vec    = self.out_layer(self.hidden_vec)
-
-
+        
         return self.out_vec
 
